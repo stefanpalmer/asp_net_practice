e76fb8ceb068f02d53b1099496d6efd2465c340c
stefanpalmer <palmerstefan@yahoo.com>
2017-07-25 23:26:16 +0200

    Add validation and update controller

--- Vidly1/Controllers/CustomersController.cs
+++ Vidly1/Controllers/CustomersController.cs
@@ -25,17 +25,44 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
-                MembershipTypes = membershipTypes
+                Customer = new Customer(),
+                MembershipTypes = _context.MembershipTypes.ToList()
             };
             return View("CustomerForm", viewModel);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customerInDb,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            { 
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
@@ -44,7 +71,7 @@ namespace Vidly.Controllers
 
                 customerInDb.Name = customer.Name;
                 customerInDb.BirthDate = customer.BirthDate;
-                customerInDb.MembershipType = customer.MembershipType;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
                 customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
             }
 
@@ -55,35 +82,20 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            var customersInDb = _context.Customers.Include(c => c.MembershipType).ToList();
            
-            return View(customers);
+            return View(customersInDb);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            var customerInDb = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
-            if (customer == null)
+            if (customerInDb == null)
                 return HttpNotFound();
 
-            return View(customer);
+            return View(customerInDb);
         }
 
-        public ActionResult Edit(int id)
-        {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
-
-            if (customer == null)
-                return HttpNotFound();
-
-            var viewModel = new CustomerFormViewModel
-            {
-                Customer = customer,
-                MembershipTypes = _context.MembershipTypes.ToList()
-            };
-
-            return View("CustomerForm", viewModel);
-        }
     }
 }
---
+++ Vidly1/Migrations/201707252115418_AddValidationAttributes.cs
@@ -0,0 +1,16 @@
+namespace Vidly1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddValidationAttributes : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
